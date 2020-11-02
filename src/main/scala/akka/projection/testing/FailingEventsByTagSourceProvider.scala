b7d56301dca54e8a23641a9bf5b6e52b9ee5799f
Christopher Batey <christopher.batey@gmail.com>
2020-11-02 15:44:25 +0000

    Initial test setup (#1)

---
+++ src/main/scala/akka/projection/testing/FailingEventsByTagSourceProvider.scala
@@ -0,0 +1,67 @@
+package akka.projection.testing
+
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+import akka.NotUsed
+import akka.actor.typed.ActorSystem
+import akka.annotation.ApiMayChange
+import akka.persistence.query.NoOffset
+import akka.persistence.query.Offset
+import akka.persistence.query.PersistenceQuery
+import akka.persistence.query.scaladsl.EventsByTagQuery
+import akka.projection.eventsourced.EventEnvelope
+import akka.projection.scaladsl.SourceProvider
+import akka.stream.scaladsl.Source
+
+import scala.util.Random
+import scala.util.control.NoStackTrace
+
+@ApiMayChange
+object FailingEventsByTagSourceProvider {
+
+  def eventsByTag[Event](
+                          system: ActorSystem[_],
+                          readJournalPluginId: String,
+                          tag: String): SourceProvider[Offset, EventEnvelope[Event]] = {
+
+    val eventsByTagQuery =
+      PersistenceQuery(system).readJournalFor[EventsByTagQuery](readJournalPluginId)
+
+    new FailingEventsByTagSourceProvider(eventsByTagQuery, tag, system)
+  }
+
+  private class FailingEventsByTagSourceProvider[Event](
+                                                  eventsByTagQuery: EventsByTagQuery,
+                                                  tag: String,
+                                                  system: ActorSystem[_])
+    extends SourceProvider[Offset, EventEnvelope[Event]] {
+    implicit val executionContext: ExecutionContext = system.executionContext
+    private val failEvery = {
+      system.settings.config.getString("test.projection-failure-every").toLowerCase() match {
+        case "off" => Int.MaxValue
+        case _ => system.settings.config.getInt("test.projection-failure-every")
+      }
+    }
+
+    override def source(offset: () => Future[Option[Offset]]): Future[Source[EventEnvelope[Event], NotUsed]] =
+      offset().map { offsetOpt =>
+        val offset = offsetOpt.getOrElse(NoOffset)
+        eventsByTagQuery
+          .eventsByTag(tag, offset)
+          .map { env =>
+            if (failEvery != Int.MaxValue && Random.nextInt(failEvery) == 1) {
+              throw new RuntimeException(s"Persistence id ${env.persistenceId} sequence nr ${env.sequenceNr} offset ${env.offset} Restart the stream!") with NoStackTrace
+            }
+            env
+          }
+          .map(env => EventEnvelope(env))
+
+      }
+
+    override def extractOffset(envelope: EventEnvelope[Event]): Offset = envelope.offset
+
+    override def extractCreationTime(envelope: EventEnvelope[Event]): Long = envelope.timestamp
+  }
+}
+
---
+++ src/main/scala/akka/projection/testing/TestValidation.scala
@@ -0,0 +1,79 @@
+package akka.projection.testing
+
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import javax.sql.DataSource
+
+import scala.concurrent.duration.FiniteDuration
+
+object TestValidation {
+
+  sealed trait ValidationResult
+  case object Pass extends ValidationResult
+  case object Fail extends ValidationResult
+  case object NoChange extends ValidationResult
+
+
+  def apply(testName: String, nrProjections: Int, expectedNrEvents: Long, timeout: FiniteDuration, source: DataSource): Behavior[String] = {
+    import scala.concurrent.duration._
+    Behaviors.setup { ctx =>
+      // Don't do this at home
+      var checksSinceChange = 0
+      var previousResult: Seq[Int] = Nil
+      def validate(): ValidationResult = {
+        val results: Seq[Int] = (0 until nrProjections).map { projectionId =>
+          val connection = source.getConnection
+          try {
+            val resultSet = connection.createStatement().executeQuery(s"select count(*) from events where name = '$testName' and projection_id = $projectionId")
+            if (resultSet.next()) {
+              val count = resultSet.getInt("count")
+              ctx.log.info("Test [{}]. Projection id: [{}]. Expected {} got {}!", testName, projectionId, expectedNrEvents, count)
+              count
+            } else {
+              throw new RuntimeException("Expected single row")
+            }
+          } finally {
+            connection.close()
+          }
+        }
+
+        if (results.forall(_ == expectedNrEvents)) {
+          Pass
+        } else {
+          if (results == previousResult) {
+           checksSinceChange += 1
+          } else {
+            checksSinceChange = 0
+          }
+          previousResult = results
+          if (checksSinceChange > 20) { // no change for 40 seconds
+            NoChange
+          } else {
+            Fail
+          }
+        }
+      }
+
+      Behaviors.withTimers { timers =>
+        timers.startTimerAtFixedRate("test", 2.seconds)
+        timers.startSingleTimer("timeout", timeout)
+        Behaviors.receiveMessage {
+          case "test" =>
+            validate() match {
+              case Pass =>
+                ctx.log.info("TestPhase: Validated. Stopping")
+                Behaviors.stopped
+              case Fail =>
+                Behaviors.same
+              case NoChange =>
+                ctx.log.error("TestPhase: Results are not changing. Stopping")
+                Behaviors.stopped
+            }
+          case "timeout" =>
+            ctx.log.error("TestPhase: Timout out")
+            Behaviors.stopped
+        }
+      }
+    }
+  }
+}
