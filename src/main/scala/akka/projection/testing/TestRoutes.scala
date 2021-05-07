fd512a96f5278b1030ea30db7fba5c7b6a4d4af3
Patrik Nordwall <patrik.nordwall@gmail.com>
2021-05-07 08:45:43 +0200

    Add route to activate actors

--- src/main/scala/akka/projection/testing/TestRoutes.scala
+++ src/main/scala/akka/projection/testing/TestRoutes.scala
@@ -22,7 +22,6 @@ import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import akka.persistence.jdbc.testkit.scaladsl.SchemaUtils
 import akka.projection.testing.Main.Journal
 import akka.stream.alpakka.cassandra.scaladsl.CassandraSessionRegistry
 import akka.util.Timeout
@@ -45,13 +44,16 @@ object TestRoutes {
       concurrentActors: Int,
       timeout: Int)
 
+  case class ActivateActors(name: String, nrActors: Int)
+
   case class Response(testName: String, expectedMessages: Long)
 
   implicit val runTestFormat: RootJsonFormat[RunTest] = jsonFormat6(RunTest)
+  implicit val activateActorsFormat: RootJsonFormat[ActivateActors] = jsonFormat2(ActivateActors)
   implicit val testResultFormat: RootJsonFormat[Response] = jsonFormat2(Response)
 }
 
-class TestRoutes(loadGeneration: ActorRef[LoadGeneration.RunTest], dataSource: DataSource, journal: Journal)(
+class TestRoutes(loadGeneration: ActorRef[LoadGeneration.Command], dataSource: DataSource, journal: Journal)(
     implicit val system: ActorSystem[_]) {
 
   private val log = LoggerFactory.getLogger(classOf[TestRoutes])
@@ -146,6 +148,14 @@ class TestRoutes(loadGeneration: ActorRef[LoadGeneration.RunTest], dataSource: D
                 case Failure(t) =>
                   complete(StatusCodes.InternalServerError, s"test failed to start: " + t.getMessage)
               }
+          }
+        }
+      },
+      post {
+        path("activate") {
+          entity(as[ActivateActors]) { activateActors =>
+            loadGeneration ! LoadGeneration.ActivateActors(activateActors.name, activateActors.nrActors)
+            complete(StatusCodes.Accepted)
           }
         }
       })
