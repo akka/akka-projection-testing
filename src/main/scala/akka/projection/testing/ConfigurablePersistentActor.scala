23b97b40134644cb456ab9b4dd2d67bf326780ca
Patrik Nordwall <patrik.nordwall@gmail.com>
2023-02-08 10:43:49 +0100

    Stop message, and stop when done
    * To avoid passivation of "active" entities since
      they store many messages by looping messages to self
      without involving sharding

--- src/main/scala/akka/projection/testing/ConfigurablePersistentActor.scala
+++ src/main/scala/akka/projection/testing/ConfigurablePersistentActor.scala
@@ -18,6 +18,8 @@ package akka.projection.testing
 
 import java.util.concurrent.ThreadLocalRandom
 
+import scala.concurrent.duration._
+
 import akka.Done
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ ActorRef, ActorSystem, Behavior }
@@ -32,12 +34,14 @@ object ConfigurablePersistentActor {
   val Key: EntityTypeKey[Command] = EntityTypeKey[Command]("configurable")
 
   def init(settings: EventProcessorSettings, system: ActorSystem[_]): ActorRef[ShardingEnvelope[Command]] = {
-    ClusterSharding(system).init(Entity(Key)(ctx => apply(settings, ctx.entityId)).withRole("write-model"))
+    ClusterSharding(system).init(Entity(Key)(ctx => apply(settings, ctx.entityId)).withRole("write-model").withStopMessage(Stop))
   }
 
-  trait Command
+  sealed trait Command
 
   final case class WakeUp(testName: String) extends Command with CborSerializable
+
+  case object Stop extends Command
 
   final case class PersistAndAck(
       totalEvents: Long,
@@ -88,10 +92,10 @@ object ConfigurablePersistentActor {
               if (state.eventsProcessed == totalEvents) {
                 ctx.log.debug("Finished persisting {} events. Replying to {}", totalEvents, replyTo)
                 replyTo ! StatusReply.ack()
+                Effect.stop()
                 Effect.none
               } else {
                 val msg = s"${toPersist}-${state.eventsProcessed}"
-                ctx.log.trace("persisting: {}", msg)
 
                 Effect.persist(Event(testName, payload = msg, data = randomPayload(bytesPerEvent))).thenRun { _ =>
                   ctx.self ! InternalPersist(totalEvents, testName, toPersist, bytesPerEvent, replyTo)
@@ -100,6 +104,9 @@ object ConfigurablePersistentActor {
             case WakeUp(testName) =>
               ctx.log.debug("WakeUp {}", ctx.self.path.name)
               Effect.none
+            case Stop =>
+              ctx.log.debug("Stop {}", ctx.self.path.name)
+              Effect.stop()
           },
         (state, _) => state.copy(eventsProcessed = state.eventsProcessed + 1)).withTagger(event =>
         (0 until settings.nrProjections).map { projection =>
